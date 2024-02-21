fa5775e78b0b464d157edc8e9e721fbd2a534c05
Dino Infantino <dino.infantino@displayplan.com>
2024-02-21 17:55:24 +0000

    Initial repo

---
+++ index.ts
@@ -0,0 +1,9 @@
+﻿import NSpell from 'nspell';
+import aff from "dictionary-en-gb/index.aff";
+import dic from "dictionary-en-gb/index.dic";
+
+export function Dino() {
+    const spell = NSpell(aff, dic);
+    const isCorrect = spell.correct('colour');
+    console.log(isCorrect);
+}
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+﻿const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+    entry: {
+        richedit: "./index.ts",
+        'richedit.min': "./index.ts"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader'
+            },
+            {
+                test: /\.(aff|dic)$/,
+                type: 'asset/source'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts']
+    },
+    experiments: {
+        outputModule: true
+    },
+    output: {
+        filename: '[name].js',
+        library: {
+            type: 'module'
+        }
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin({test: /\.min\.js$/})]
+    }
+};
